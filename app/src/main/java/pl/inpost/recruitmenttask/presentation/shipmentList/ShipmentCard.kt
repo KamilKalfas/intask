74739ecc99122905df652b54028add9470cf32da
Kamil Kalfas <kamilkalfas@gmail.com>
2024-12-05 22:31:03 +0100

    - updated card layout

--- app/src/main/java/pl/inpost/recruitmenttask/presentation/shipmentList/ShipmentCard.kt
+++ app/src/main/java/pl/inpost/recruitmenttask/presentation/shipmentList/ShipmentCard.kt
@@ -1,17 +1,27 @@
 package pl.inpost.recruitmenttask.presentation.shipmentList
 
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -20,7 +30,11 @@ import pl.inpost.recruitmenttask.domain.model.Customer
 import pl.inpost.recruitmenttask.domain.model.Operations
 import pl.inpost.recruitmenttask.domain.model.Shipment
 import pl.inpost.recruitmenttask.domain.model.ShipmentStatus
+import pl.inpost.recruitmenttask.domain.model.ShipmentType
 import pl.inpost.recruitmenttask.presentation.theme.InPostRecruitmentTaskTheme
+import pl.inpost.recruitmenttask.presentation.theme.brandingYellow
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 @Composable
 fun ShipmentCard(
@@ -28,8 +42,12 @@ fun ShipmentCard(
     onClick: () -> Unit,
     modifier: Modifier
 ) {
+    val cardState = rememberShipmentCardState(
+        shipment = shipment
+    )
     ShipmentCardContent(
-        shipment = shipment,
+        number = shipment.number,
+        state = cardState,
         onClick = onClick,
         modifier = modifier
     )
@@ -37,48 +55,134 @@ fun ShipmentCard(
 
 @Composable
 private fun ShipmentCardContent(
-    shipment: Shipment,
+    number: String,
+    state: ShipmentCardState,
     onClick: () -> Unit,
     modifier: Modifier
 ) {
-    val senderName = remember(shipment.sender) {
-        shipment.sender.let { sender ->
-            sequenceOf(sender.name, sender.email, sender.phoneNumber).firstOrNull()
-        }
-    }
-
     Column(
         modifier = modifier
             .background(MaterialTheme.colors.background)
             .padding(horizontal = 20.dp)
             .padding(top = 16.dp, bottom = 8.dp)
     ) {
-        Text("NR PRZESYLKI")
-        Text(shipment.number)
+        ShipmentNumberRow(shipmentNumber = number, deliveryIcon = state.deliveryIcon)
         Spacer(modifier = Modifier.height(16.dp))
-        Text("STATUS")
-        Text(shipment.status.toString())
+        ShipmentStatusRow(
+            shipmentStatus = stringResource(id = state.statusLabel),
+            shipmentFormattedDate = state.shipmentFormattedDate,
+            showExtendedStatus = state.showExtendedStatus
+        )
         Spacer(modifier = Modifier.height(16.dp))
-        Text("NADAWCA")
-        Text(senderName ?: stringResource(id = R.string.sender_unknown))
+        ShipmentSenderRow(senderName = state.senderName, onClick = onClick)
+    }
+}
+
+@Composable
+private fun ShipmentNumberRow(
+    shipmentNumber: String,
+    @DrawableRes deliveryIcon: Int,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .heightIn(min = 40.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Column {
+            Text(text = stringResource(id = R.string.shipment_card_number_label))
+            Text(text = shipmentNumber)
+        }
+        Icon(
+            painter = painterResource(id = deliveryIcon),
+            contentDescription = null,
+        )
+    }
+}
+
+@Composable
+private fun ShipmentStatusRow(
+    shipmentStatus: String,
+    shipmentFormattedDate: String,
+    showExtendedStatus: Boolean,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Column {
+            Text(text = stringResource(id = R.string.shipment_card_status_label))
+            Text(shipmentStatus)
+        }
+        if (showExtendedStatus) {
+            Column {
+                Text(text = shipmentStatus)
+                Text(text = shipmentFormattedDate)
+            }
+        }
+    }
+}
+
+@Composable
+private fun ShipmentSenderRow(
+    senderName: String,
+    onClick: () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .heightIn(min = 40.dp)
+            .clickable {
+                onClick()
+            }
+    ) {
+        Text(
+            modifier = Modifier.align(Alignment.TopStart),
+            text = stringResource(id = R.string.shipment_card_sender_label)
+        )
+        Text(
+            modifier = Modifier.align(Alignment.BottomStart),
+            text = senderName
+        )
+        Row(
+            modifier = Modifier.align(Alignment.BottomEnd),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                color = Color.Black,
+                text = stringResource(id = R.string.shipment_card_more_button_label),
+            )
+            Icon(
+                painter = painterResource(id = R.drawable.ic_arrow),
+                tint = MaterialTheme.colors.brandingYellow,
+                contentDescription = ""
+            )
+        }
     }
 }
 
 @Preview
 @Composable
 private fun preview_content() {
+    val shipment = Shipment(
+        number = "16730345345597442248333",
+        type = ShipmentType.LOCKER,
+        sender = Customer("sender@example.com", null, null),
+        status = ShipmentStatus.READY_TO_PICKUP,
+        operations = Operations(false, false, false, false, false, false),
+        pickupDate = ZonedDateTime.parse("2022-11-05T04:56:07Z", DateTimeFormatter.ISO_OFFSET_DATE_TIME),
+        expireDate = null,
+        storedDate = null,
+    )
     InPostRecruitmentTaskTheme {
         Surface {
             ShipmentCardContent(
-                shipment = Shipment(
-                    number = "16730345345597442248333",
-                    sender = Customer("sender@example.com", null, null),
-                    status = ShipmentStatus.READY_TO_PICKUP,
-                    operations = Operations(false, false, false, false, false, false),
-                    pickupDate = null,
-                    expireDate = null,
-                    storedDate = null,
-                ),
+                number = shipment.number,
+                state = ShipmentCardState(shipment = shipment, resources = LocalContext.current.resources),
                 onClick = {},
                 modifier = Modifier.fillMaxWidth()
             )
---
+++ app/src/main/java/pl/inpost/recruitmenttask/presentation/shipmentList/ShipmentCardState.kt
@@ -0,0 +1,78 @@
+package pl.inpost.recruitmenttask.presentation.shipmentList
+
+import android.content.res.Resources
+import androidx.annotation.StringRes
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.intl.LocaleList
+import pl.inpost.recruitmenttask.R
+import pl.inpost.recruitmenttask.domain.model.Shipment
+import pl.inpost.recruitmenttask.domain.model.ShipmentStatus
+import pl.inpost.recruitmenttask.domain.model.ShipmentType
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Locale
+
+@Composable
+fun rememberShipmentCardState(shipment: Shipment): ShipmentCardState {
+    val resources = LocalContext.current.resources
+    return remember(shipment) {
+        ShipmentCardState(shipment = shipment, resources = resources)
+    }
+}
+
+@Stable
+class ShipmentCardState(
+    shipment: Shipment,
+    resources: Resources,
+    locale: Locale = Locale.getDefault()
+) {
+    val senderName = shipment.sender.let { sender ->
+        sequenceOf(sender.name, sender.email, sender.phoneNumber).firstOrNull()
+    } ?: resources.getString(R.string.unknown)
+
+    val deliveryIcon = when (shipment.type) {
+        ShipmentType.LOCKER -> R.drawable.ic_paczkomat
+        ShipmentType.COURIER -> R.drawable.ic_kurier
+    }
+
+    @StringRes
+    val statusLabel: Int = when (shipment.status) {
+        ShipmentStatus.ADOPTED_AT_SORTING_CENTER -> R.string.status_adopted_at_sorting_center
+        ShipmentStatus.SENT_FROM_SORTING_CENTER -> R.string.status_sent_from_sorting_center
+        ShipmentStatus.ADOPTED_AT_SOURCE_BRANCH -> R.string.status_adopted_at_source_branch
+        ShipmentStatus.SENT_FROM_SOURCE_BRANCH -> R.string.status_sent_from_source_branch
+        ShipmentStatus.AVIZO -> R.string.status_avizo
+        ShipmentStatus.CONFIRMED -> R.string.status_confirmed
+        ShipmentStatus.CREATED -> R.string.status_created
+        ShipmentStatus.DELIVERED -> R.string.status_delivered
+        ShipmentStatus.OTHER -> R.string.status_other
+        ShipmentStatus.OUT_FOR_DELIVERY -> R.string.status_out_for_delivery
+        ShipmentStatus.PICKUP_TIME_EXPIRED -> R.string.status_pickup_time_expired
+        ShipmentStatus.READY_TO_PICKUP -> R.string.status_ready_to_pickup
+        ShipmentStatus.RETURNED_TO_SENDER -> R.string.status_returned_to_sender
+        ShipmentStatus.UNKNOWN -> R.string.unknown
+    }
+
+    val showExtendedStatus = shipment.status in listOf(ShipmentStatus.READY_TO_PICKUP, ShipmentStatus.DELIVERED, ShipmentStatus.PICKUP_TIME_EXPIRED)
+
+    val shipmentFormattedDate = when (shipment.status) {
+        ShipmentStatus.READY_TO_PICKUP -> shipmentFormattedDate(requireNotNull(shipment.storedDate), locale)
+        ShipmentStatus.DELIVERED -> shipmentFormattedDate(requireNotNull(shipment.pickupDate), locale)
+        ShipmentStatus.PICKUP_TIME_EXPIRED -> shipmentFormattedDate(requireNotNull(shipment.expireDate), locale)
+        else -> ""
+    }
+
+    private fun shipmentFormattedDate(zonedDateTime: ZonedDateTime, locale: Locale): String {
+        val dayNameFormatter = DateTimeFormatter.ofPattern("E", locale)
+        val dayName = zonedDateTime.format(dayNameFormatter).lowercase(locale)
+        val dateFormatter = DateTimeFormatter.ofPattern("dd.MM.yy", locale)
+        val date = zonedDateTime.format(dateFormatter)
+        val timeFormatter = DateTimeFormatter.ofPattern("HH:mm", locale)
+        val time = zonedDateTime.format(timeFormatter)
+
+        return "$dayName | $date | $time"
+    }
+}
