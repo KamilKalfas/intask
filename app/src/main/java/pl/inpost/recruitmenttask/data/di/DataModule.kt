0fec8cad1a4a23bc0daa5a69f8f0084881557e58
Kamil Kalfas <kamilkalfas@gmail.com>
2024-12-06 15:41:38 +0100

    - updated data module with dao providers - used in memory database room builder for easy testing while working on db

--- app/src/main/java/pl/inpost/recruitmenttask/data/di/DataModule.kt
+++ app/src/main/java/pl/inpost/recruitmenttask/data/di/DataModule.kt
@@ -1,11 +1,17 @@
 package pl.inpost.recruitmenttask.data.di
 
 import android.content.Context
+import androidx.room.Room
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import pl.inpost.recruitmenttask.data.database.AppDatabase
+import pl.inpost.recruitmenttask.data.database.CustomerDao
+import pl.inpost.recruitmenttask.data.database.ShipmentsDao
+import pl.inpost.recruitmenttask.data.database.entity.OperationsDao
+import pl.inpost.recruitmenttask.data.datasource.ShipmentsLocalDataSource
 import pl.inpost.recruitmenttask.data.datasource.ShipmentsRemoteDataSource
 import pl.inpost.recruitmenttask.data.network.adapters.ShipmentStatusTypeAdapter
 import pl.inpost.recruitmenttask.data.network.adapters.ZoneDateTimeTypeAdapter
@@ -35,5 +41,45 @@ object DataModule {
     @Provides
     fun provideRemoteDataSource(api: ShipmentApi): ShipmentsRemoteDataSource {
         return ShipmentsRemoteDataSource(api)
+    }
+
+    @Provides
+    fun provideLocalDataSource(
+        shipmentsDao: ShipmentsDao,
+        customerDao: CustomerDao,
+        operationsDao: OperationsDao
+    ): ShipmentsLocalDataSource {
+        return ShipmentsLocalDataSource(
+            shipmentsDao = shipmentsDao,
+            operationsDao = operationsDao,
+            customersDao = customerDao,
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext context: Context): AppDatabase {
+        return Room.inMemoryDatabaseBuilder(
+            context,
+            AppDatabase::class.java,
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideShipmentsDao(db: AppDatabase): ShipmentsDao {
+        return db.shipmentsDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideCustomerDao(db: AppDatabase): CustomerDao {
+        return db.customersDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideOperationsDao(db: AppDatabase): OperationsDao {
+        return db.operationsDao()
     }
 }
