69ec88c943409f936bc0c47618e04b1d3da108d2
Kamil Kalfas <kamilkalfas@gmail.com>
2024-12-06 16:07:37 +0100

    - implemented insert all

--- app/src/main/java/pl/inpost/recruitmenttask/data/datasource/ShipmentsLocalDataSource.kt
+++ app/src/main/java/pl/inpost/recruitmenttask/data/datasource/ShipmentsLocalDataSource.kt
@@ -1,8 +1,12 @@
 package pl.inpost.recruitmenttask.data.datasource
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import pl.inpost.recruitmenttask.data.database.CustomerDao
 import pl.inpost.recruitmenttask.data.database.ShipmentsDao
 import pl.inpost.recruitmenttask.data.database.entity.OperationsDao
+import pl.inpost.recruitmenttask.data.database.entity.ShipmentEntity
+import pl.inpost.recruitmenttask.data.mapper.toEntity
 import pl.inpost.recruitmenttask.domain.model.Shipment
 
 class ShipmentsLocalDataSource(
@@ -11,6 +15,14 @@ class ShipmentsLocalDataSource(
     private val customersDao: CustomerDao
 ) {
     suspend fun insertAll(shipments: List<Shipment>) {
-
+        withContext(Dispatchers.IO) {
+            val entities = mutableListOf<ShipmentEntity>()
+            shipments.forEach {
+                val operationId = operationsDao.insert(it.operations.toEntity())
+                val senderId = customersDao.insert(it.sender.toEntity())
+                entities.add(it.toEntity(senderId = senderId, operationsId = operationId))
+            }
+            shipmentsDao.insertAll(entities)
+        }
     }
 }
