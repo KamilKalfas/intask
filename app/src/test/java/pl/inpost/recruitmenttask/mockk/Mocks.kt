3f499cacd5787e4ddb9738dbc85c89a78c84b9e2
Kamil Kalfas <kamilkalfas@gmail.com>
2024-12-06 15:30:51 +0100

    fixed missing method for tests

--- app/src/test/java/pl/inpost/recruitmenttask/mockk/Mocks.kt
+++ app/src/test/java/pl/inpost/recruitmenttask/mockk/Mocks.kt
@@ -1,9 +1,15 @@
 package pl.inpost.recruitmenttask.mockk
 
+import pl.inpost.recruitmenttask.data.model.CustomerDto
+import pl.inpost.recruitmenttask.data.model.OperationsDto
+import pl.inpost.recruitmenttask.data.model.ShipmentDto
+import pl.inpost.recruitmenttask.data.model.ShipmentStatusDto
+import pl.inpost.recruitmenttask.data.model.ShipmentTypeDto
 import pl.inpost.recruitmenttask.domain.model.Customer
 import pl.inpost.recruitmenttask.domain.model.Operations
 import pl.inpost.recruitmenttask.domain.model.Shipment
 import pl.inpost.recruitmenttask.domain.model.ShipmentStatus
+import pl.inpost.recruitmenttask.domain.model.ShipmentType
 import java.time.ZonedDateTime
 
 fun fakeShipment(
@@ -27,5 +33,39 @@ fun fakeShipment(
         expandAvizo = false,
         endOfWeekCollection = false,
     ),
-    sender = Customer(null, null, null)
+    sender = Customer(null, null, null),
+    type = ShipmentType.LOCKER
+)
+
+fun fakeShipmentDto(
+    number: String,
+    senderName: String,
+    operationHighlight: Boolean
+) = ShipmentDto(
+    number = number,
+    shipmentType = ShipmentTypeDto.PARCEL_LOCKER,
+    status = ShipmentStatusDto.DELIVERED,
+    eventLog = emptyList(),
+    openCode = null,
+    expiryDate = null,
+    storedDate = null,
+    pickUpDate = ZonedDateTime.now(),
+    receiver = CustomerDto(
+        email = null,
+        phoneNumber = null,
+        name = null
+    ),
+    sender = CustomerDto(
+        email = null,
+        phoneNumber = null,
+        name = senderName
+    ),
+    operations = OperationsDto(
+        manualArchive = false,
+        delete = false,
+        collect = false,
+        highlight = operationHighlight,
+        expandAvizo = false,
+        endOfWeekCollection = false
+    )
 )
