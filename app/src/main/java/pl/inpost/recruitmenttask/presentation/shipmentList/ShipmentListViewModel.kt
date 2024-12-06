c022f934ebca9fd5a2091e289e13314e993963ff
Kamil Kalfas <kamilkalfas@gmail.com>
2024-12-07 00:27:04 +0100

    and thissss, and now dropdown jumps around cuz reasons.

--- app/src/main/java/pl/inpost/recruitmenttask/presentation/shipmentList/ShipmentListViewModel.kt
+++ app/src/main/java/pl/inpost/recruitmenttask/presentation/shipmentList/ShipmentListViewModel.kt
@@ -1,5 +1,6 @@
 package pl.inpost.recruitmenttask.presentation.shipmentList
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -8,11 +9,10 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import pl.inpost.recruitmenttask.R
+import pl.inpost.recruitmenttask.domain.exceptions.Failure
 import pl.inpost.recruitmenttask.domain.model.Shipment
 import pl.inpost.recruitmenttask.domain.usecase.ArchiveShipmentUseCase
 import pl.inpost.recruitmenttask.domain.usecase.GetShipmentsGroupedByHighlightedUseCase
-import pl.inpost.recruitmenttask.domain.usecase.GroupedShipmentsResult
 import javax.inject.Inject
 
 @HiltViewModel
@@ -32,22 +32,20 @@ class ShipmentListViewModel @Inject constructor(
         // dispatchers IO because we pretend this is network call
         viewModelScope.launch(Dispatchers.IO) {
             _state.update { old -> old.copy(isLoading = true) }
-
-            when (val result = shipmentsGroupedByHighlightedUseCase.execute()) {
-                is GroupedShipmentsResult.Error -> {
-                    // TODO() add some error UI information
+            try {
+                val result = shipmentsGroupedByHighlightedUseCase.execute()
+                _state.update { old ->
+                    old.copy(
+                        shipmentsReadyToPickup = result.readyToPickup,
+                        shipmentsRest = result.rest
+                    )
                 }
-
-                is GroupedShipmentsResult.Success -> {
-                    _state.update { old ->
-                        old.copy(
-                            isLoading = false,
-                            shipmentsReadyToPickup = result.readyToPickup,
-                            shipmentsRest = result.rest
-                        )
-                    }
-                }
+            } catch (failure: Failure) {
+                Log.d("ShipmentListViewModel", "Error $failure")
             }
+            _state.update { old -> old.copy(isLoading = false,) }
+        }
+    }
 
     fun onArchivePackage(shipmentNumber: String) {
         viewModelScope.launch(Dispatchers.IO) {
